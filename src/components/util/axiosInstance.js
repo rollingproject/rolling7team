32df2702ee1f80b82b8428a55161507ffb12e172
bumsly <65000254+bumsly@users.noreply.github.com>
2023-12-14 20:05:36 +0900

    axiosInstance 이름 수정

--- src/components/util/axiosInstance.js
+++ src/components/util/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_ADDRESS } from "../../post/data-access-post/constant";
 
-export const getRecipients = axios.create({
+export const axiosInstance = axios.create({
   baseURL: API_ADDRESS,
   header: {
     "Content-Type": "application/json",
