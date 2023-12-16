7617e304a0e4588abb1fff3a5ea0fc32ad13083f
sooki88 <sookhee9n@gmail.com>
2023-12-16 10:40:48 +0900

    feat: PostByIdEdit에서 recipient의 롤링페이퍼 전체 삭제 기능 추가

--- src/pages/PostByIdEditPage.jsx
+++ src/pages/PostByIdEditPage.jsx
@@ -9,7 +9,7 @@ import { CardById } from "../components/ui-card-by-id/CardById";
 import styles from "./PostByIdEditPage.module.scss";
 import { axiosInstance } from "../components/util/axiosInstance";
 
-const SAMPLE = 1088;
+const SAMPLE = 1227;
 
 export const PostByIdEditPage = ({ selectedId = SAMPLE }) => {
   const navigate = useNavigate();
@@ -17,7 +17,6 @@ export const PostByIdEditPage = ({ selectedId = SAMPLE }) => {
 
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [deleteId, setDeleteId] = useState(null);
 
   const { loading, data } = useGetMessagesList(selectedId);
   const { results } = data || {};
@@ -30,9 +29,14 @@ export const PostByIdEditPage = ({ selectedId = SAMPLE }) => {
     }
   }, [results, loading]);
 
-  // const deleteMessage = useDeleteMessages(deleteId);
+  const handleDeleteRecipient = (id) => {
+    const response = axiosInstance.delete(`recipients/${id}/`);
+    if (!response) return;
 
-  const handleDelete = (postId) => {
+    navigate("/list");
+  };
+
+  const handleDeleteMessage = (postId) => {
     const response = axiosInstance.delete(`messages/${postId}/`);
     if (!response) return;
 
@@ -42,7 +46,12 @@ export const PostByIdEditPage = ({ selectedId = SAMPLE }) => {
   return (
     <div className={styles.background}>
       <div className={styles.container}>
-        <button className={styles.button}>삭제하기</button>
+        <button
+          className={styles.button}
+          onClick={() => handleDeleteRecipient(selectedId)}
+        >
+          삭제하기
+        </button>
       </div>
       <CardByIdList>
         {posts?.map((post) => (
@@ -50,111 +59,10 @@ export const PostByIdEditPage = ({ selectedId = SAMPLE }) => {
             key={post.id}
             {...post}
             showDeleteButton={true}
-            onDelete={() => handleDelete(post.id)}
+            onDelete={() => handleDeleteMessage(post.id)}
           />
         ))}
       </CardByIdList>
     </div>
   );
 };
-
-// 여기까지
-
-// import { useGetPostsById } from "../post-by-id/data-access-post-by-id/useGetPostsById";
-// import { CardByIdList } from "../components/ui-card-by-id-list/CardByIdList";
-// import { CardById } from "../components/ui-card-by-id/CardById";
-// import styles from "./PostByIdEditPage.module.scss";
-// import { useParams, useNavigate } from "react-router-dom";
-// import { useDeleteMessages } from "../post/data-access-post/messageApi";
-// import { useCallback, useEffect, useState } from "react";
-
-// const SAMPLE = 1088; // 임시 id로 나중에 list 페이지에서 prop을 받아야 함.
-
-// export const PostByIdEditPage = ({ selectedId = SAMPLE }) => {
-//   const navigate = useNavigate();
-//   const { recipientId } = useParams();
-
-//   const [posts, setPosts] = useState(null);
-//   const postData = useGetPostsById(selectedId);
-//   const handleLoad = useCallback(() => {
-//     const { data } = postData;
-//     const sortedPosts = data?.results.sort((a, b) => b.createdAt - a.createdAt);
-//     setPosts(sortedPosts);
-//   }, [postData]);
-
-//   useEffect(() => {
-//     handleLoad();
-//   }, []);
-
-//   const handleDeleteRecipient = () => {
-//     navigate("/list");
-//   };
-
-//   const deletePostApi = useDeleteMessages();
-//   const handleDeletePost = (postId) => {
-//     const result = deletePostApi(postId);
-//     if (!result) return;
-
-//     setPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
-//   };
-
-//   return (
-//     <div className={styles.background}>
-//       <div className={styles.container}>
-//         <button className={styles.button} onClick={handleDeleteRecipient}>
-//           삭제하기
-//         </button>
-//       </div>
-//       <CardByIdList>
-//         {posts?.map((post) => (
-//           <CardById
-//             key={post.id}
-//             {...post}
-//             showDeleteButton={true}
-//             onDelete={handleDeletePost}
-//           />
-//         ))}
-//       </CardByIdList>
-//     </div>
-//   );
-// };
-
-// const axiosInstance = axios.create({
-//   baseURL: API_ADDRESS,
-//   header: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// const useApiRequest = (apiFunction) => {
-//   const { loading, error, data } = useAsync(apiFunction);
-//   return { loading, error, data };
-//   // data에는 count, next, previous, results가 포함.
-// };
-
-// const useDeleteMessages = (messageId) => {
-//   const deleteMessages = () => axiosInstance.delete(`messages/${messageId}/`);
-//   return useApiRequest(deleteMessages);
-// };
-
-// 팀미팅때 물어볼 것
-// const SAMPLE = 1088;
-
-// export const PostByIdEditPage = (selectedId = SAMPLE) => {
-
-//   const { loading, error, data } = useGetMessagesList(selectedId);
-
-//   const { results } = data || {};
-
-//   return <div>{results}</div>;
-// };
-
-// delete실패코드
-
-// const deleteMessage = useDeleteMessages(deleteId);
-
-// const handleDelete = (postId) => {
-//   setDeleteId(postId);
-//   deleteMessage();
-//   setPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
-// };
