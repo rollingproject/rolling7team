e17ac17503aa08d770c03a45db5a978a05120873
bumsly <65000254+bumsly@users.noreply.github.com>
2023-12-17 15:44:43 +0900

    app 파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import NavigationBar from "./components/ui-navigation-bar/NavigationBar";
-import { useGetRecipientsList } from "./post/data-access-post/recipientsApi";
 
 function App() {
   return (
