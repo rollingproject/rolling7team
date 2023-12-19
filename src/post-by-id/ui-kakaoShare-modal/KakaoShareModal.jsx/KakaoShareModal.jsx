3b89f74c7227d726f3d74e485f97df3deb9cf959
ashtonjo <hee576@nate.com>
2023-12-19 14:57:41 +0900

    fix: Profile 디자인 원형으로 변경

---
+++ src/post-by-id/ui-kakaoShare-modal/KakaoShareModal.jsx/KakaoShareModal.jsx
@@ -0,0 +1,11 @@
+import styles from "./KakaoShareModal.module.scss";
+
+function KakaoShareModal() {
+  return (
+    <>
+    <div className={}></div>
+    </>
+  )
+}
+
+export default KakaoShareModal;
