16f97ad4158fd4b486508ff203ebc0655e991370
sooki88 <sookhee9n@gmail.com>
2023-12-19 14:52:22 +0900

    chore: merge 전

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -15,9 +15,17 @@ function SelectBackground({ backgroundType, userData, setUserData }) {
   function handleSetUserData(e) {
     const selected = e.target.htmlFor;
     if (backgroundType === "color") {
-      setUserData({ ...userData, backgroundColor: selected, backgroundImageURL: "" });
+      setUserData({
+        ...userData,
+        backgroundColor: selected,
+        backgroundImageURL: "",
+      });
     } else {
-      setUserData({ ...userData, backgroundColor: null, backgroundImageURL: selected });
+      setUserData({
+        ...userData,
+        backgroundColor: null,
+        backgroundImageURL: selected,
+      });
     }
     console.log(selected);
   }
@@ -28,8 +36,17 @@ function SelectBackground({ backgroundType, userData, setUserData }) {
         {backgrounds.map((background, index) => {
           return (
             <Fragment key={background}>
-              <input id={background} type="radio" name="background" defaultChecked={index == 0} />
-              <label htmlFor={background} onClick={handleSetUserData} className={styles.form__label__background}></label>
+              <input
+                id={background}
+                type="radio"
+                name="background"
+                defaultChecked={index == 0}
+              />
+              <label
+                htmlFor={background}
+                onClick={handleSetUserData}
+                className={styles.form__label__background}
+              ></label>
             </Fragment>
           );
         })}
@@ -43,20 +60,41 @@ function SelectColorOrImg({ setBackgroundType, userData, setUserData }) {
     const backgroundType = e.target.value;
     setBackgroundType(backgroundType);
     if (backgroundType === "color") {
-      setUserData({ ...userData, backgroundColor: "beige", backgroundImageURL: "" });
+      setUserData({
+        ...userData,
+        backgroundColor: "beige",
+        backgroundImageURL: "",
+      });
     } else {
-      setUserData({ ...userData, backgroundColor: null, backgroundImageURL: bgImg1 });
+      setUserData({
+        ...userData,
+        backgroundColor: null,
+        backgroundImageURL: bgImg1,
+      });
     }
   }
 
   return (
     <>
       <div className={styles.category}>
-        <input type="radio" id="selectColorButton" value="color" onClick={handleClick} name="category" defaultChecked />
+        <input
+          type="radio"
+          id="selectColorButton"
+          value="color"
+          onClick={handleClick}
+          name="category"
+          defaultChecked
+        />
         <label className={styles.category__label} htmlFor="selectColorButton">
           컬러
         </label>
-        <input type="radio" id="selectImgButton" value="image" onClick={handleClick} name="category" />
+        <input
+          type="radio"
+          id="selectImgButton"
+          value="image"
+          onClick={handleClick}
+          name="category"
+        />
         <label className={styles.category__label} htmlFor="selectImgButton">
           이미지
         </label>
@@ -67,7 +105,12 @@ function SelectColorOrImg({ setBackgroundType, userData, setUserData }) {
 
 export function PostPage() {
   const [backgroundType, setBackgroundType] = useState("color");
-  const [userData, setUserData] = useState({ team: "7", name: "", backgroundColor: "beige", backgroundImageURL: "" });
+  const [userData, setUserData] = useState({
+    team: "7",
+    name: "",
+    backgroundColor: "beige",
+    backgroundImageURL: "",
+  });
 
   useEffect(() => {
     console.log(userData);
@@ -79,9 +122,19 @@ export function PostPage() {
         To.
       </InputName>
       <span className={styles.span}>배경화면을 선택해 주세요.</span>
-      <p className={styles.p}>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
-      <SelectColorOrImg userData={userData} setUserData={setUserData} setBackgroundType={setBackgroundType} />
-      <SelectBackground userData={userData} setUserData={setUserData} backgroundType={backgroundType} />
+      <p className={styles.p}>
+        컬러를 선택하거나, 이미지를 선택할 수 있습니다.
+      </p>
+      <SelectColorOrImg
+        userData={userData}
+        setUserData={setUserData}
+        setBackgroundType={setBackgroundType}
+      />
+      <SelectBackground
+        userData={userData}
+        setUserData={setUserData}
+        backgroundType={backgroundType}
+      />
       <CreatePost userData={userData}>생성하기</CreatePost>
     </form>
   );
