e52535ea86d1bbecb60a2d71a7d3fde22661d376
Hyunsoo_kim <red_panda119@naver.com>
2023-12-22 02:14:48 +0900

    Card.jsx 파일명변경 => Card_list.jsx, 비어있는  feature-layout 폴더 삭제

--- src/components/ui-card/Card_list.jsx
+++ src/components/ui-card/Card_list.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./Card.module.scss";
+import styles from "./Card_list.module.scss";
 import { Link } from "react-router-dom";
 
 export default function Card({ item }) {
