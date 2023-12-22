22ac7dd48a6c4d9035994a39f2872689696ee35f
Hyunsoo_kim <red_panda119@naver.com>
2023-12-22 12:44:01 +0900

    add css hover

--- src/components/ui-card/Card_list.jsx
+++ src/components/ui-card/Card_list.jsx
@@ -9,7 +9,9 @@ export default function Card({ item }) {
   const isImg = {};
 
   backgroundImageURL
-    ? (isImg.backgroundImage = `linear-gradient(180deg, rgba(0, 0, 0, 0.54) 0%, rgba(0, 0, 0, 0.54) 100%), url(${backgroundImageURL})`)
+    ? (isImg.backgroundImage = 
+      `linear-gradient(180deg, rgba(0, 0, 0, 0.54) 0%, rgba(0, 0, 0, 0.54) 100%),
+      url(${backgroundImageURL})`)
     : {};
 
   useEffect(() => {
@@ -29,7 +31,7 @@ export default function Card({ item }) {
       setEmojiList((prevState) => [
         ...prevState,
         ...emojiListDataResult.results,
-      ]); 
+      ]);
     })();
   }, []);
 
